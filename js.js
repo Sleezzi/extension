d3615787a421e6dd16f4abd8e4765ede390a9a50
Sleezzi <93204760+Sleezzi@users.noreply.github.com>
2022-12-08 09:29:37 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,9 +1,11 @@
-var settings = {
-  "url": "https://id.twitch.tv/oauth2/token?client_id=6onacrgnxzevvd6luatv9uem1773r0&client_secret=izy71djfu0t1wt4ittfk1kz238q2up&grant_type=client_credentials",
-  "method": "POST",
-  "timeout": 0
-};
+function fetchTwitchToken() {
+  $.ajax({
+	"url": "https://id.twitch.tv/oauth2/token?client_id=6onacrgnxzevvd6luatv9uem1773r0&client_secret=izy71djfu0t1wt4ittfk1kz238q2up&grant_type=client_credentials",
+	"method": "POST",
+	"timeout": 0
+  }).done(function (response) {
+	document.body.innerHTML = '<pre style="word-wrap: break-word; white-space: pre-wrap;">{"access_token": ' + response.access_token + ', "expires_in": ' + response.expires_in + '}"<pre>';
+  });
+}
 
-$.ajax(settings).done(function (response) {
-  document.body.innerHTML = '<pre style="word-wrap: break-word; white-space: pre-wrap;">{"access_token": ' + response.access_token + ', "expires_in": ' + response.expires_in + '}"<pre>';
-});
+setInterval(function() { fetchTwitchToken(); }, 25000);
