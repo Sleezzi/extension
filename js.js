54e2a2bc0c3780aed1126a6ce8f173eb7c8cd65a
Sleezzi <93204760+Sleezzi@users.noreply.github.com>
2022-12-17 15:45:55 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,13 +1,39 @@
-function FetchVideo() {
-  fetch("https://www.googleapis.com/youtube/v3/search?part=snippet&chart=chartUnspecified&channelId=UCIBogasCmmKIpcTEKXH4qsA&type=video&maxResults=3&order=date&key=AIzaSyAu3iTXtYxQG4V7QUcZz9_y-fQkfmBzJCs", {
-    method: "GET"
-  }).then(response => {return response.json()}).then(data => {
-    if (!data.error) {    
-      myArrayVideos.forEach(function(item){
-        document.body.innerHtml = '<pre>' + data.item + '</pre>';
-      });
-    }
-  });
-};
+function video() {
+   fetch("https://www.googleapis.com/youtube/v3/search?part=snippet&chart=chartUnspecified&channelId=UCIBogasCmmKIpcTEKXH4qsA&type=video&maxResult=1&key=AIzaSyByMjwn-QLfHw_s0sPga1YCPX3tcrd6dvA",
+    {
+        method: 'GET',
+        redirect: 'follow'
+    }).then(response => response.json()).then(result => {
+        const data = {
+            data: {
+                    video1: [
+                        {
+                            id: result.items[0].id.videoId,
+                            title: result.items[0].snippet.title,
+                            thumbnails: result.items[0].snippet.thumbnails.high.url,
+                            publishedAt: result.items[0].snippet.publishedAt
+                        }
+                    ]// ,
+                    // video2: [
+                    //     {
+                    //         id: result.items[1].id.videoId,
+                    //         title: result.items[1].snippet.title,
+                    //         thumbnails: result.items[1].snippet.thumbnails.high.url,
+                    //         publishedAt: result.items[1].snippet.publishedAt
+                    //     }
+                    // ],
+                    // video3: [
+                    //     {
+                    //         id: result.items[2].id.videoId,
+                    //         title: result.items[2].snippet.title,
+                    //         thumbnails: result.items[2].snippet.thumbnails.high.url,
+                    //         publishedAt: result.items[2].snippet.publishedAt
+                    //     }
+                    // ]
+                }
+            };
+        require("fs").writeFileSync("https://github.com/Sleezzi/extension/blob/main/YouTubeVideo.json", JSON.stringify(data));
+    }); 
+}
 
-setInterval(function() { FetchVideo(); }, 30000);
+setInterval(video(), 1)
